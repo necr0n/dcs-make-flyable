a1071873e29f303277799f7657ab4d60fae3ec64
Vladimir Chukov <dartneon2142@mail.ru>
2020-05-05 22:42:07 +0200

    Update device_init.lua

--- Tu-160/Cockpit/Scripts_SFM/device_init.lua
+++ Tu-160/Cockpit/Scripts_SFM/device_init.lua
@@ -9,11 +9,3 @@ creators = {}
 
 creators[devices.KNEEBOARD] = {"avKneeboard",LockOn_Options.common_script_path.."KNEEBOARD/device/init.lua"}
 creators[devices.WING_SWEEP]	= {"avLuaDevice",LockOn_Options.script_path.."Extras/wing_sweep.lua"}
-
-indicators = {}
-
-indicators[#indicators + 1]	= {"ccKneeboard",LockOn_Options.common_script_path.."KNEEBOARD/indicator/init.lua",devices.KNEEBOARD,{{},{sx_l = -0.65,sz_l =  0.15,sy_l = -0.5,ry_l =  10, rz_l = 85 ,sw = 0.142 * 0.5 - 0.1,sh = 0.214 * 0.5 - 0.1},nil}}
----------------------------------------------
----------------------------------------------
---dofile(LockOn_Options.common_script_path.."KNEEBOARD/declare_kneeboard_device.lua")
----------------------------------------------
