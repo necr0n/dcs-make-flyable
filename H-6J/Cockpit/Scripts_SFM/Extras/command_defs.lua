5323e551fbdecb15a6b87ab7df3e052419acc070
Vladimir Chukov <dartneon2142@mail.ru>
2021-09-19 02:28:49 +0200

    Add files via upload

---
+++ H-6J/Cockpit/Scripts_SFM/Extras/command_defs.lua
@@ -0,0 +1,52 @@
+
+
+Keys =
+{
+	PlanePickleOn	= 350,
+	PlanePickleOff	= 351,
+    PlaneChgWeapon  = 101,
+    PlaneChgTargetNext = 102,    -- iCommandPlaneChangeTarget
+    PlaneModeNAV    = 105,
+    PlaneModeBVR    = 106,
+    PlaneModeVS     = 107,
+    PlaneModeBore   = 108,
+    PlaneModeGround = 111,
+
+	Canopy = 71,
+	
+	PlaneAirBrake = 73,		
+	PlaneAirBrakeOn = 147,
+	PlaneAirBrakeOff = 148,	
+	
+	PlaneFlaps = 72,
+	PlaneFlapsOn = 145, -- Fully down
+	PlaneFlapsOff = 146, -- Fully up
+    	
+	PlaneGear = 68,						-- Шасси
+	PlaneGearUp	= 430,
+	PlaneGearDown = 431,
+		
+	-- LeftEngineStart = 311,			-- ?????? ?????? ?????????
+	-- RightEngineStart = 312,			-- ?????? ??????? ?????????
+
+	-- LeftEngineStop = 313,				-- ?????????? ?????? ?????????
+	-- RightEngineStop = 314,			-- ?????????? ??????? ?????????
+
+	PowerOnOff = 315,
+
+    --[[   -- Do not use the built-in altimeter adjustments, they have internal SSM affects on the altimeter that we cannot limit
+    AltimeterPressureIncrease = 316,  
+    AltimeterPressureDecrease = 317,
+    AltimeterPressureStop = 318,        ]]--
+
+    PlaneLightsOnOff = 175,
+    PlaneHeadlightOnOff = 328,
+
+    PowerGeneratorLeft = 711,
+    PowerGeneratorRight = 712,
+
+    BatteryPower = 1073,        -- iCommandBatteryPower
+
+    PlaneChgTargetPrev = 1315,   -- iCommandPlaneUFC_STEER_DOWN
+
+}
